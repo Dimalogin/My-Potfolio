af5e2528d1c7e92f6740a311fdb2f35011f08071
DimaLoginovskiy <remen.2000@mail.ru>
2022-12-15 01:34:41 +0300

    feat: change file index.js add functionality

--- my-portfolio/index.js
+++ my-portfolio/index.js
@@ -1,1 +1,35 @@
+import Router from "./scripts/router.js";
 
+(function router() {
+  Router.init();
+})();
+
+// Side Navigation
+
+const sideNavMobile = document.querySelector(".header__sidenav--mobile");
+const openButton = sideNavMobile.querySelector(".open__btn--mobile");
+
+const sideNavMobilePanel = sideNavMobile.querySelector(
+  ".sidenav__panel--mobile"
+);
+
+const closeButton = sideNavMobile.querySelector(".close__btn--mobile");
+
+const allLinksMobile = sideNavMobilePanel.querySelectorAll(
+  ".header__link--mobile"
+);
+
+openButton.addEventListener("click", openPanel);
+closeButton.addEventListener("click", closePanel);
+
+for (const iterator of allLinksMobile) {
+  iterator.addEventListener("click", closePanel);
+}
+
+function openPanel() {
+  sideNavMobilePanel.style.width = "340px";
+}
+
+function closePanel() {
+  sideNavMobilePanel.style.width = "0px";
+}
